0fb9e53deeb28bfec65a3e40409b4cb93e1ca64c
Emma_Tang <aimengfeiyuan@gmail.com>
2024-06-18 11:13:42 +0900

    フォントkiwi-maruをtailwindの設定に実装した

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kiwi-maru': ['"Kiwi Maru"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
 
